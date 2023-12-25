2cc8ac602fcacc47186dc1278884005b73bb318d
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2023-12-26 02:37:27 +0700

    update testcase

--- scripts/test.js
+++ scripts/test.js
@@ -1,101 +1,81 @@
+// command line: npx hardhat run scripts/test.js --network sepolia
+
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const fs = require("fs");
+const { id } = require("ethers/lib/utils");
+const utils = ethers.utils;
+
+require("dotenv").config();
+
+tokenAddress = "0x9b6C42fd47b7b334Ae8BF4faAa4C0e5C01e44dd5"; // Replace with actual token address
+tokenInvestedAddress = "0x1e0516962D607D9c54817A742A22bb60976C5Aed"; // Replace with actual token address
+launchpadAddress = "0x45A44bB1f252fe261277Ab803c87574817377aF0"; // Replace with actual launchpad address
 
 describe("Launchpad Contract", function () {
-  let Launchpad, launchpad;
-  let owner, user1, user2;
-  let tokenAddress, tokenInvested;
-  let projectDetails;
-  let projectId;
-
-  before(async function () {
-    Launchpad = await ethers.getContractFactory("Launchpad");
-    [owner, user1, user2] = await ethers.getSigners();
-
-    tokenAddress = "0x9b6C42fd47b7b334Ae8BF4faAa4C0e5C01e44dd5"; // Replace with actual token address
-    tokenInvested = "0x1e0516962D607D9c54817A742A22bb60976C5Aed"; // Replace with actual token address
-  });
-
   beforeEach(async function () {
-    launchpad = await Launchpad.deploy();
-    await launchpad.deployed();
+    Launchpad = await ethers.getContractFactory("Launchpad");
+    launchpad = await Launchpad.attach(launchpadAddress);
+    Token = await ethers.getContractFactory("Token");
+    token = await Token.attach(tokenAddress);
+    TokenInvested = await ethers.getContractFactory("USDT");
+    tokenInvested = await TokenInvested.attach(tokenInvestedAddress);
+    [owner] = await ethers.getSigners();
 
-    projectDetails = {
-      tokenAddress,
-      tokenInvested,
-      tokenPrice: ethers.utils.parseUnits("1", 18),
-      maxTokenPerUser: ethers.utils.parseUnits("100", 18),
-      maxCapacity: ethers.utils.parseUnits("1000", 18),
-      minTokenPerUser: ethers.utils.parseUnits("10", 18),
-      startTime: Math.floor(Date.now() / 1000) + 3600, // 1 hour later
-      endTime: Math.floor(Date.now() / 1000) + 86400, // 1 day later
-    };
-
-    // Owner creates a project
-    await launchpad.createProject(
-      projectDetails.tokenAddress,
-      projectDetails.tokenInvested,
-      projectDetails.tokenPrice,
-      projectDetails.maxTokenPerUser,
-      projectDetails.maxCapacity,
-      projectDetails.minTokenPerUser,
-      projectDetails.startTime,
-      projectDetails.endTime
-    );
-
-    projectId = await launchpad.totalProject();
+    console.log("launchpad address:", launchpad.address);
+    console.log("token address:", token.address);
+    console.log("tokenInvested address:", tokenInvested.address);
+    console.log("owner address:", owner.address);
   });
 
-  describe("Creating a project", function () {
+  describe("create a project", function () {
     it("should allow owner to create a project", async function () {
-      await expect(
-        launchpad.createProject(
-          projectDetails.tokenAddress,
-          projectDetails.tokenInvested,
-          projectDetails.tokenPrice,
-          projectDetails.maxTokenPerUser,
-          projectDetails.maxCapacity,
-          projectDetails.minTokenPerUser,
-          projectDetails.startTime,
-          projectDetails.endTime
-        )
-      ).to.emit(launchpad, "ProjectListed");
+      const result = await launchpad.createProject(
+        token.address,
+        tokenInvested.address,
+        ethers.utils.parseUnits("1"),
+        ethers.utils.parseUnits("100"),
+        ethers.utils.parseUnits("1000"),
+        ethers.utils.parseUnits("10"),
+        Math.floor(Date.now() / 1000) + 20, // 20 seconds from now
+        Math.floor(Date.now() / 1000) + 86400 // 1 day from now
+      );
+      console.log(result);
     });
   });
+  // describe("Investing in a project", function () {
+  //   it("should allow users to invest in a project", async function () {
+  //     const investmentAmount = ethers.utils.parseUnits("50", 18);
 
-  describe("Investing in a project", function () {
-    it("should allow users to invest in a project", async function () {
-      const investmentAmount = ethers.utils.parseUnits("50", 18);
+  //     await expect(launchpad.connect(user1).invest(projectId, investmentAmount))
+  //       .to.emit(launchpad, "ProjectInvested")
+  //       .withArgs(projectId, user1.address, investmentAmount);
+  //   });
+  // });
 
-      await expect(launchpad.connect(user1).invest(projectId, investmentAmount))
-        .to.emit(launchpad, "ProjectInvested")
-        .withArgs(projectId, user1.address, investmentAmount);
-    });
-  });
+  // describe("Withdraw from a project", function () {
+  //   it("should allow project owner to withdraw", async function () {
+  //     // Forward time to after project end
+  //     await ethers.provider.send("evm_increaseTime", [86400 + 1]);
+  //     await ethers.provider.send("evm_mine");
 
-  describe("Withdraw from a project", function () {
-    it("should allow project owner to withdraw", async function () {
-      // Forward time to after project end
-      await ethers.provider.send("evm_increaseTime", [86400 + 1]);
-      await ethers.provider.send("evm_mine");
+  //     await expect(launchpad.connect(owner).withdrawProject(projectId)).to.emit(
+  //       launchpad,
+  //       "ProjectWithdrawn"
+  //     ); // Assuming this event exists
+  //   });
+  // });
 
-      await expect(launchpad.connect(owner).withdrawProject(projectId)).to.emit(
-        launchpad,
-        "ProjectWithdrawn"
-      ); // Assuming this event exists
-    });
-  });
+  // describe("Claiming from a project", function () {
+  //   it("should allow investors to claim after project ends", async function () {
+  //     // Forward time to after project end
+  //     await ethers.provider.send("evm_increaseTime", [86400 + 1]);
+  //     await ethers.provider.send("evm_mine");
 
-  describe("Claiming from a project", function () {
-    it("should allow investors to claim after project ends", async function () {
-      // Forward time to after project end
-      await ethers.provider.send("evm_increaseTime", [86400 + 1]);
-      await ethers.provider.send("evm_mine");
-
-      await expect(launchpad.connect(user1).claimProject(projectId)).to.emit(
-        launchpad,
-        "ProjectClaimed"
-      ); // Assuming this event exists
-    });
-  });
+  //     await expect(launchpad.connect(user1).claimProject(projectId)).to.emit(
+  //       launchpad,
+  //       "ProjectClaimed"
+  //     ); // Assuming this event exists
+  //   });
+  // });
 });
